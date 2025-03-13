0bedfbfedd6ea10c506e99993ea5164b9eae1b92
a_hirano <kou105328@gmail.com>
2025-03-13 22:01:10 +0900

    add readme

--- run.sh
+++ run.sh
@@ -4,13 +4,13 @@ echo "<select program>"
 echo ""
 echo "1 → 与えられた.kファイルからmonolis形式のメッシュ(block)を作成"
 echo ""
-echo "2 → map情報作成のテスト実行 & 作成されたmap情報をグリッドへ可視化"
+echo "2 → monolis形式のsubcell読み取り & map情報の作成 & meso meshの作成"
 echo ""
-echo "3 → monolis形式のsubcell読み取り & map情報の作成 & meso meshの作成"
+echo "3 → 生成されたmeso meshのVTKファイル作成"
 echo ""
-echo "99 → 生成されたmeso meshのVTKファイル作成"
+echo "99 → map情報作成のテスト実行 & 作成されたmap情報をグリッドへ可視化"
 echo ""
-read -p "プログラムの選択（対応する整数値を入力） : " case
+read -p "プログラムの選択 : " case
 echo ""
 
 if [ $case -eq 1 ]; then
@@ -18,14 +18,14 @@ if [ $case -eq 1 ]; then
     echo "python3 ./ssrc/01_make_blocks_main.py"
     echo ""
     python3 ./src/01_make_blocks_main.py v3
-elif [ $case -eq 2 ];then
+elif [ $case -eq 99 ];then
     read -p "meso mesh におけるテープ数 p の入力 (2以上) : p = " case2
     echo ""
     echo "**************** RUN PROGRAMS ****************"
     echo "python3 ./ssrc/02_blocks_main.py"
     echo ""
     python3 ./src/02_place_blocks_main.py $case2
-elif [ $case -eq 3 ];then
+elif [ $case -eq 2 ];then
     read -p "meso mesh におけるテープ数 p の入力 (2以上) : p = " case2
     # echo ""
     # read -p "kdtree構造体で定義する各節点のバウンディングボックス（立方体）の一辺の長さ : inf = " case3
@@ -34,6 +34,6 @@ elif [ $case -eq 3 ];then
     echo "python3 ./src/03_create_mesomesh.py"
     echo ""
     python3 ./src/03_create_mesomesh.py $case2 #$case3
-elif [ $case -eq 99 ];then
+elif [ $case -eq 3 ];then
     python3 ./src/99_output_vtk.py
 fi
